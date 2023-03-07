2566e5c7b37613d38cb41a7f3384d4d81c85df8b
Sergey Serebryakov <sergey@serebryakov.info>
2023-03-07 00:40:34 -0500

    Update bio

--- fillers/MainFiller.php
+++ fillers/MainFiller.php
@@ -10,11 +10,11 @@ class MainFiller extends AbstractFiller {
 
         $town_link = "https://www1.nyc.gov";
         //$spbu_link = "http://www.spbu.ru";
-        $company_link = "https://www.hudsonrivertrading.com";
+        // $company_link = "https://www.hudsonrivertrading.com";
 
         $content = str_replace("%TOWN_LINK%", $town_link, $content);
         //$content = str_replace("%SPBU_LINK%", $spbu_link, $content);
-        $content = str_replace("%COMPANY_LINK%", $company_link, $content);
+        // $content = str_replace("%COMPANY_LINK%", $company_link, $content);
 
         $this -> content = $content;
     }
